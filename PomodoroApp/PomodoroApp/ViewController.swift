2d536321b63b158a203cbfd3edf46f08631aecb2
Furkan Yükcü <furkan.yukcu@turkcell.com.tr>
2023-11-06 12:15:26 +0300

    first commit

---
+++ PomodoroApp/PomodoroApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PomodoroApp
+//
+//  Created by Furkan Yükcü on 6.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PomodoroApp/PomodoroAppUITests/PomodoroAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PomodoroAppUITestsLaunchTests.swift
+//  PomodoroAppUITests
+//
+//  Created by Furkan Yükcü on 6.11.2023.
+//
+
+import XCTest
+
+final class PomodoroAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
